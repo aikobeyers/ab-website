008b6b83b1b8f0c96608a09cdb5784e2cad1dd30
Aiko Beyers <aikobeyers@hotmail.com>
2022-05-11 22:40:58 +0200

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomepageComponent} from "./components/homepage/homepage.component";
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomepageComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: HomepageComponent //TODO PageNotFoundComponent
+  }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
