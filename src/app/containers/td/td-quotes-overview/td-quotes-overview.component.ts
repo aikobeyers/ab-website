ab11c6028b8fc71a5d001fe1ce4f9b44fda1398b
aikobeyers <aikobeyers@hotmail.com>
2026-01-25 22:16:34 +0100

    Initial display of quotes

--- src/app/containers/td/td-quotes-overview/td-quotes-overview.component.ts
+++ src/app/containers/td/td-quotes-overview/td-quotes-overview.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { TdQuotesService } from '../../../services/td-quotes.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-td-quotes-overview',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './td-quotes-overview.component.html',
   styleUrl: './td-quotes-overview.component.scss'
 })
 export class TdQuotesOverviewComponent implements OnInit {
 
+  public quotes: any;
 
-  public constructor(private readonly tdQuotesService: TdQuotesService) { }
+  public constructor(private readonly tdQuotesService: TdQuotesService) {
+    this.quotes = toSignal(this.tdQuotesService.getAllTdQuotes());
+  }
 
   public ngOnInit(): void {
-    this.tdQuotesService.getAllTdQuotes().subscribe((quotes) => {
-      console.log(quotes);
-    });
+    console.log('test');
+    
   }
 }
