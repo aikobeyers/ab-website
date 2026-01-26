c2101446305349defb42299739ee214db47d6999
aikobeyers <aikobeyers@hotmail.com>
2026-01-26 21:40:15 +0100

    set title

--- src/app/containers/td/td-quotes-overview/td-quotes-overview.component.ts
+++ src/app/containers/td/td-quotes-overview/td-quotes-overview.component.ts
@@ -4,22 +4,25 @@ import { toSignal } from '@angular/core/rxjs-interop';
 import { TdQuoteCardComponent } from "./components/td-quote-card/td-quote-card.component";
 import { CommonModule } from '@angular/common';
 import { MatIcon } from "@angular/material/icon";
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-td-quotes-overview',
+  providers: [Title],
   imports: [TdQuoteCardComponent, CommonModule, MatIcon],
   templateUrl: './td-quotes-overview.component.html',
   styleUrl: './td-quotes-overview.component.scss'
 })
 export class TdQuotesOverviewComponent implements OnInit {
   private readonly tdQuotesService = inject(TdQuotesService);
+  private readonly titleService = inject(Title);
 
   public quotes = toSignal(
     this.tdQuotesService.getAllTdQuotes(), { initialValue: [] }
   );
 
   public ngOnInit(): void {
-    console.log('Hqllo');
+    this.titleService.setTitle('TD Quotes');
   
   }
 }
