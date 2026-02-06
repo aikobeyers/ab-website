a243c05409573ded57b06fe38a48460444dcf11c
aikobeyers <aikobeyers@hotmail.com>
2026-02-06 12:49:55 +0100

    Step 1 of game

--- src/app/containers/td/td-quote-game/td-quote-game.component.ts
+++ src/app/containers/td/td-quote-game/td-quote-game.component.ts
@@ -5,6 +5,7 @@ import { TdQuotesService } from '../../../services/td-quotes.service';
 import { take } from 'rxjs';
 import { SkeletonComponent } from "../../../components/skeleton/skeleton.component";
 import { TdQuoteWithId } from '../../../models/TdQuote';
+import { FiltersStore } from '../../../stores/filters.store';
 
 @Component({
   selector: 'app-td-quote-game',
@@ -15,16 +16,20 @@ import { TdQuoteWithId } from '../../../models/TdQuote';
 export class TdQuoteGameComponent implements OnDestroy {
   private readonly document = inject(DOCUMENT);
   private readonly tdQuotesService = inject(TdQuotesService);
+  private readonly store = inject(FiltersStore);
 
   public isOpen = false;
   public isLoading = signal(false);
 
   public randomQuote = signal<TdQuoteWithId | null>(null);
+  public gameStage = signal<'quote' | 'authors'>('quote');
+  public authors = this.store.authors;
 
   public closeGameEmitter = output<void>();
 
   openGame(): void {
     this.isLoading.set(true);
+    this.gameStage.set('quote');
     this.setOpen(true);
 
     this.tdQuotesService.getRandomQuote()
@@ -33,6 +38,10 @@ export class TdQuoteGameComponent implements OnDestroy {
         this.isLoading.set(false);
         this.randomQuote.set(quote);
       });
+  }
+
+  showAuthors(): void {
+    this.gameStage.set('authors');
   }
 
   closeGame(): void {
